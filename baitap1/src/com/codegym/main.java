effcd81be862266db41ab3986fa5e3a0915c2abb
bao2706 <giabaolop8@gmail.com>
2026-05-22 14:58:32 +0900

    Add files via upload
    [Thực hành] triển khai interface 'Comparable' cho các lớp hình học (v.2023)

---
+++ baitap1/src/com/codegym/main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+import java.util.Arrays;
+
+
+public class main {
+    public static void main(String[] args){
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0]=new ComparableCircle(3.4);
+        circles[1]=new ComparableCircle();
+        circles[2]=new ComparableCircle(3.5,"red",false);
+        System.out.println("Ham trc khi sort");
+        for( ComparableCircle circle: circles){
+            System.out.println(circle);
+        }
+        Arrays.sort(circles);
+        System.out.println("Ham sau khi sort");
+        for( ComparableCircle circle: circles){
+            System.out.println(circle);
+        }
+    }
+}
